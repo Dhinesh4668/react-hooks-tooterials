e0e923f23bbfde859c3d6849f9b3c928bf2348e8
Dhinesh Kumar <87540180+Dhinesh4668@users.noreply.github.com>
2023-02-08 21:34:40 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App =()=> {
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1>dhinesh <code>kumar</code></h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+  const [count ,setCount] = useState(1)
+
+
+  function countfun() {
+    setCount(count + 1)
+  }
+  return(
+    <div className="App" style={{alignItems: "center", justifyContent: "center", display: "flex"}}>
+      <h1>sample button</h1>
+<button  onClick={countfun}>count button:{count}</button>
     </div>
   )
 }
